d2f0247edf2f0b87bfb64b3f003a443a5104df98
CodeZhangBorui <zhangborui.2008@foxmail.com>
2024-03-31 19:57:28 +0800

    feat: Add auto-migrate config feature to setup.py, Add Geetest dependency

---
+++ geetest.py
@@ -0,0 +1,58 @@
+import json
+import logging
+import hmac
+import requests
+
+with open('config.json', 'r') as f:
+    config = json.load(f)['geetest']
+enabled = config['enable']
+captcha_id = config['captcha_id']
+captcha_key = config['captcha_key']
+api_server = config['api_server']
+
+def verify_test(lot_number='', captcha_output='', pass_token='', gen_time=''):
+    if not enabled:
+        return {
+            "result": "success",
+            "reason": "Geetest is disabled."
+        }
+    # 生成签名
+    # 生成签名使用标准的hmac算法，使用用户当前完成验证的流水号lot_number作为原始消息message，使用客户验证私钥作为key
+    # 采用sha256散列算法将message和key进行单向散列生成最终的签名
+    lotnumber_bytes = lot_number.encode()
+    prikey_bytes = captcha_key.encode()
+    sign_token = hmac.new(prikey_bytes, lotnumber_bytes, digestmod='SHA256').hexdigest()
+    
+    # 上传校验参数到极验二次验证接口, 校验用户验证状态
+    query = {
+        "lot_number": lot_number,
+        "captcha_output": captcha_output,
+        "pass_token": pass_token,
+        "gen_time": gen_time,
+        "sign_token": sign_token,
+    }
+    url = f"{api_server}/validate?captcha_id={captcha_id}"
+    # 注意处理接口异常情况，当请求极验二次验证接口异常或响应状态非200时做出相应异常处理
+    # 保证不会因为接口请求超时或服务未响应而阻碍业务流程
+    try:
+        res = requests.post(url, query)
+        assert res.status_code == 200
+        msg = res.json()
+        if msg['result'] == 'success':
+            logging.info("Geetest captcha passed successfully.")
+            return {
+                "result": "success",
+                "reason": msg['reason']
+            }
+        else:
+            return {
+                "result": "fail",
+                "reason": msg['reason']
+            }
+    except Exception as e:
+        return {
+            "result": "error",
+            "reason": "Failed to verify captcha. Please try again later.",
+            "exception": e
+        }
+        
--- setup.py
+++ setup.py
@@ -5,19 +5,50 @@ import hashlib
 import importlib
 from datetime import datetime
 
+config_version = 2
+config = {
+    "secret_key": "ReplaceWithYourSecretKey",
+    "database": "database.db",
+    "uploads": {
+        "upload_folder": "./uploads",
+        "max_content_length": 2048
+    },
+    "geetest": {
+        "enable": False,
+        "captcha_id": "ReplaceWithYourCaptchaID",
+        "captcha_key": "ReplaceWithYourCaptchaKey",
+    }
+}
+database_version = 1
+
 try:
     with open('setup.lock', 'r') as f:
         setup_lock = json.load(f)
     if setup_lock['setup']:
         setup_time = datetime.fromtimestamp(setup_lock['time']).strftime('%Y-%m-%d %H:%M:%S')
         print(f"Error while initalize setup progress:\nsetup.lock exist. FileDock may be installed at {setup_time}.\nIf you believe this is an error, please remove setup.lock and continue.")
+        if config_version != setup_lock['config_version']:
+            with open('config.json', 'r') as f:
+                old_config = json.load(f)
+            for key, value in old_config.items():
+                if key not in config:
+                    config[key] = value
+            with open('config.json', 'w') as f:
+                json.dump(config, f)
+            print(
+                f"Your config.json version is {config_version}, but setup.lock is {setup_lock['config_version']}. We've tried to update your config.json, please check and edit if you need.")
+            with open('setup.lock', 'w') as f:
+                setup_lock['config_version'] = config_version
+                json.dump(setup_lock, f)
+        if database_version != setup_lock['database_version']:
+            print(f"Your database version is {database_version}, but setup.lock is {setup_lock['database_version']}. We recommend you to update database.\nTo migrate database, please visit FileDock Wiki.")
 except:
     # Install dependencies
     dependencies = {
         'flask': 'flask',
         'flask_uploads': 'git+https://github.com/riad-azz/flask-uploads',
     }
-    print(f"\t[Step 1] Install {len(dependencies)} dependencies.")
+    print(f"[Step 1] Install {len(dependencies)} dependencies.")
     for key, value in dependencies.items():
         try:
             importlib.import_module(key)
@@ -27,15 +58,7 @@ except:
             system(f"pip install {value}")
 
     # Edit config.json
-    print(f"\t[Step 2] Create config.json")
-    config = {
-        "secret_key": "ReplaceWithYourSecretKey",
-        "database": "database.db",
-        "uploads": {
-            "upload_folder": "./uploads",
-            "max_content_length": 2048
-        }
-    }
+    print(f"[Step 2] Create config.json")
     with open('config.json', 'w') as f:
         json.dump(config, f)
     print("Created config file, please check and edit if you need.")
@@ -44,48 +67,48 @@ except:
         config = json.load(f)
 
     # Create database
-    print(f"\t[Step 3] Create database.")
+    print(f"[Step 3] Create database.")
     database = config['database']
     sqlite = [
         """create table users
-(
-    uid      integer                not null
-        constraint users_pk
-            primary key
-        constraint users_uniqpk
-            unique,
-    username TEXT default 'User'    not null,
-    password text                   not null,
-    "group"  text default 'default' not null
-);""",
+        (
+        uid      integer                not null
+            constraint users_pk
+                primary key
+            constraint users_uniqpk
+                unique,
+        username TEXT default 'User'    not null,
+        password text                   not null,
+        "group"  text default 'default' not null
+        );""",
         """create table uploads
-(
-    uuid         TEXT               not null
-        constraint uploads_pk
-            primary key
-        constraint uploads_uniqpk
-            unique,
-    filename     TEXT               not null,
-    code         integer            not null,
-    upload_time  integer            not null,
-    keep_time    integer default -1 not null,
-    upload_user  integer            not null,
-    receive_user integer default null
-);""",
-"""create table groups
-(
-    name     text                not null
-        constraint groups_pk
-            primary key
-        constraint groups_uniqpk
-            unique,
-    operator integer default 0   not null,
-    max_size integer default 512 not null
-);""",
+        (
+        uuid         TEXT               not null
+            constraint uploads_pk
+                primary key
+            constraint uploads_uniqpk
+                unique,
+        filename     TEXT               not null,
+        code         integer            not null,
+        upload_time  integer            not null,
+        keep_time    integer default -1 not null,
+        upload_user  integer            not null,
+        receive_user integer default null
+        );""",
+        """create table groups
+        (
+        name     text                not null
+            constraint groups_pk
+                primary key
+            constraint groups_uniqpk
+                unique,
+        operator integer default 0   not null,
+        max_size integer default 512 not null
+        );""",
         """INSERT INTO groups (name, operator, max_size)
-VALUES ('default', 0, 512);""",
+        VALUES ('default', 0, 512);""",
         """INSERT INTO groups (name, operator, max_size)
-VALUES ('operator', 1, 512);""",
+        VALUES ('operator', 1, 512);""",
     ]
     conn = sqlite3.connect(database)
     c = conn.cursor()
@@ -96,7 +119,7 @@ VALUES ('operator', 1, 512);""",
     print(f"Successfully execute {len(sqlite)} queries.")
 
     # Create default operator
-    print(f"\t[Step 4] Create default operator account.")
+    print(f"[Step 4] Create default operator account.")
     print("This user will have all permissions.")
     username = input("Username > ")
     password = input("Password > ")
@@ -113,6 +136,8 @@ VALUES ('operator', 1, 512);""",
     with open('setup.lock', 'w') as f:
         json.dump({
             'setup': True,
-            'time': int(datetime.now().timestamp())
+            'time': int(datetime.now().timestamp()),
+            'config_version': config_version,
+            'database_version': database_version
         }, f)
     print("\n\nSuccessfully configured FileDock!\nTo run server, use 'flask run' and it'll be run on port 5000.\nFor more information please visit FileDock Wiki and Flask Website.")
